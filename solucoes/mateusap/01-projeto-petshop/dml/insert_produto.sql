8ef4d01d22e102d634f2d1759e37684b2cca9661
mateusap <ma.aparecido.silva@hotmail.com>
2023-03-17 15:36:42 -0300

    Projeto PetShop - minha abordagem
    Fiz utilizando forward engineering começando pelo diagrama e transformando em tabelas do banco de dados

---
+++ solucoes/mateusap/01-projeto-petshop/dml/insert_produto.sql
@@ -0,0 +1,7 @@
+SELECT * FROM `01-projeto-petshop`.c_produto;
+insert into c_produto values (null, 'Mordedor de corda resistente','2023-03-17 09:18:09','Brinquedos','55.00','9999-12-31','1');
+insert into c_produto values 
+	(null, 'Caracol de plástico','2022-10-25 10:28:49','Brinquedos','40.00','9999-12-31','4'),
+    (null, 'Ração para cachorros sensíveis 15kg','2022-10-19 13:38:51','Alimento','180.00','2023-05-01','2'),
+    (null, 'Toca para gatos','2022-06-22 12:05:37','Tocas e Camas','120.00','9999-12-31','3'),
+    (null, 'Pelúcia Polvo','2022-11-13 11:01:06','Brinquedos','15.00','9999-12-31','5');
